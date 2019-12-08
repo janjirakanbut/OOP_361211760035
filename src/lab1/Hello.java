8856213cd559000d84bfcab3274bf6a4d47954e4
janjirakanbut <40585934+janjirakanbut@users.noreply.github.com>
2019-12-08 11:29:01 +0700

    Update Hello.java

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hell Word,I am Saroh");
-        System.out.println("Janjira");
+        System.out.println("Janjira Kanbut");
+        System.out.println("361211760035");
         System.out.println("MIT");
         System.out.println("Faculty of Management Technology");
         System.out.println("Saiyai");
