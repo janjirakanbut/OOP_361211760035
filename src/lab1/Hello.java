342aca85d8a5cc84d6be63f5b82aeff4b01fe206
Janjira <janjira.saroh@gmail.com>
2019-12-08 11:11:25 +0700

    Add message

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -5,9 +5,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hell Word,I am Saroh");
-
-
-
+        System.out.println("MIT");
+        System.out.println("Faculty of Management Technology");
 
     }//main
 
